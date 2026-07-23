ac5534f4e794dbd6c7625734032a67b9ff2d6f2d
Kimi0123 <maharjannirvan01@gmail.com>
2026-07-23 23:54:47 +0545

    contract route

--- gigflow_api/src/routes/index.ts
+++ gigflow_api/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import adminUserRoutes from "./admin-user.route";
 import authRoutes from "./auth.route";
+import contractRoutes from "./contract.route";
 import healthRoutes from "./health.route";
 import jobRoutes from "./job.route";
 
@@ -10,5 +11,6 @@ router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/admin/users", adminUserRoutes);
 router.use("/jobs", jobRoutes);
+router.use("/contracts", contractRoutes);
 
 export default router;
