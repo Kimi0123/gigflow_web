25af4ea6b95d645f56c72a13c3851df778fad762
Kimi0123 <maharjannirvan01@gmail.com>
2026-07-25 03:59:32 +0545

    updated types to show details

--- gigflow_api/src/types/user.type.ts
+++ gigflow_api/src/types/user.type.ts
@@ -1,11 +1,15 @@
 export type UserRole = "freelancer" | "client" | "admin";
 
-export interface IUser{
-  firstName : string,
-  lastName : string,
-  email: string,
-  password: string,
-  role : UserRole,
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: UserRole;
   phoneNumber: string;
   profilePicture?: string;
+  bio?: string;
+  title?: string;
+  skills?: string[];
+  cvUrl?: string;
 }
